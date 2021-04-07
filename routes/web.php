e65954cf4ec285a314dee7209c93237022896f3b
Cha <charizsalceda@gmail.com>
2021-04-08 02:55:48 +0800

    about page controller

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CSPortfolio\IndexController;
+use App\Http\Controllers\CSPortfolio\AboutController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +20,6 @@ use App\Http\Controllers\CSPortfolio\IndexController;
 // });
 
 Route::get('/', [IndexController::class, 'index']);
+
+// About
+Route::get('/about', [AboutController::class, 'index']);
